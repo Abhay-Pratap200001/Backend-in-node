8143485eec13dc6c84d96e9e4aa64d803d3f8106
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-05-04 19:13:15 +0530

    adding multer.middleware cloudinary config files

--- src/models/video.models.js
+++ src/models/video.models.js
@@ -44,11 +44,10 @@ const videoSchema = new Schema(
         }
         
 
-
-    }, 
+ }, 
 
 {timestamps:true});
 
 videoSchema.plugin(mongooseAggregatePaginate)
 
-    export const Video = mongoose.model("Video", videoSchema)
+export const Video = mongoose.model("Video", videoSchema)
